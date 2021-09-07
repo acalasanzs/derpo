ead99c4c16e97f3d6357058c5cd5198092cd5a38
acsstudios <acsstudiosdev@gmail.com>
2021-09-07 22:15:45 +0200

    First Window with icon: see https://python.hotexamples.com/es/site/file?hash=0xf31b796180be5ea0b60e9d7dc260ed81587748fd17779cf115bf775306a9f591&fullName=psychopy-master/psychopy/visual/glfwbackend.py&project=hsogo/psychopy

---
+++ main.py
@@ -0,0 +1,39 @@
+import glfw
+import os
+from win32api import GetSystemMetrics
+from PIL import Image
+
+def main():
+    # initializing glfw library
+    if not glfw.init():
+        raise Exception("glfw can not be initialized")
+
+    scale = 0.75
+    os.environ["SDL_VIDEO_CENTERED"]= '1'
+
+    black, white = (5, 6, 6), (230, 230, 230)
+
+    width , height = [int(GetSystemMetrics(i)) for i in range(2)]
+
+    win = glfw.create_window(int(width*scale),int(height*scale),"Derpo 0.0.0a",None,None)
+
+    if not win:
+        glfw.terminate()
+        raise Exception("glfw window can not be created")
+    # set pos at center
+    glfw.set_window_pos(win,int(width//2-(width*scale)/2),int(height//2-(height*scale)/2))
+
+    # make the context current
+    
+    icon = Image.open("icon.ico")
+    glfw.make_context_current(win)
+    glfw.set_window_icon(win, 1 ,icon)
+    # main loop
+    while not glfw.window_should_close(win):
+        glfw.poll_events()
+
+        glfw.swap_buffers(win)
+        
+    glfw.terminate()
+if __name__ == "__main__":
+    main()
